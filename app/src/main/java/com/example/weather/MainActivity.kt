80efcbc519f0b34fcebb915870fd65777edf9a3a
Aider <aider.dev.work@gmail.com>
2023-09-06 21:29:41 +0300

    Navigation is added

--- app/src/main/java/com/example/weather/MainActivity.kt
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.weather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+        val navController = findNavController(R.id.fragmentContainerView)
+
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment, R.id.favoriteFragment, R.id.settingFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        bottomNavigationView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener{ controller, destination, arguments ->
+            Toast.makeText(applicationContext, "Переход на фрагмент: ${destination.label}", Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/com/example/weather/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/weather/fragments/SettingsFragment.kt
@@ -5,17 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.preference.PreferenceFragmentCompat
 import com.example.weather.R
 
-class SettingFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+class SettingFragment : PreferenceFragmentCompat() {
 
-        val view = inflater.inflate(R.layout.fragment_setting, container, false)
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
 
-        return view
+        val darkModeSwitch = findPreference("enable_dark_mode") as androidx.preference.SwitchPreferenceCompat?
+        darkModeSwitch?.setOnPreferenceChangeListener { _, newValue ->
+            val isDarkModeEnabled = newValue as Boolean
+            // Обработайте изменение настройки здесь
+            true // Верните true, чтобы разрешить сохранение значения
+        }
     }
 }
